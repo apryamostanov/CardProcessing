d0da4a698cd203f8d6e2de6a82160aa84c53a9a6
apryamostanov <anton.pryamostanov@gmail.com>
2017-03-27 16:58:20 +0400

    Z

--- src/com/a9ae0b01f0ffc/VSMSGEN/main/T_vsms_base_7_main.groovy
+++ src/com/a9ae0b01f0ffc/VSMSGEN/main/T_vsms_base_7_main.groovy
@@ -5,7 +5,8 @@ import com.a9ae0b01f0ffc.VSMSGEN.implementation.T_visa_recon_generator
 class T_vsms_base_7_main extends T_vsms_base_6_util{
 
     public static void main(String... i_args) {
-        x().init_custom("./conf/commons.conf")
+        //x().init_custom("./conf/commons.conf")
+        init_custom("./src/com/a9ae0b01f0ffc/VSMSGEN/conf/commons.conf")
         T_visa_recon_generator l_visa_recon_generator = new T_visa_recon_generator()
         l_visa_recon_generator.convert_vts_log_to_ctf(c().GC_VTS_LOG_FILE, c().GC_CTF_FILE)
         l().log_info(s.Process_is_complete)
